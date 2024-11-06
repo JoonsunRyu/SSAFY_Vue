90570d614973375e3ccf0900b916edef2a390512
JoonsunRyu <rjs7289@gmail.com>
2024-11-06 17:49:25 +0900

    241106 Class Code

---
+++ Vue_Day04_SFC/vue-project/src/components/MyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>MyComponent</h3>
+    <MyComponentItem/>
+    <MyComponentItem/>
+    <MyComponentItem/>
+  </div>
+</template>
+
+<script setup>
+  // import MyComponentItem from "./MyComponentItem.vue";
+  import MyComponentItem from "@/components/MyComponentItem.vue";
+</script>
+
+<style scoped>
+
+</style>
