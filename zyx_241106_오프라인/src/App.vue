61f41aa73df1acd14c9ccf1c3fd8ecf646717212
JoonsunRyu <rjs7289@gmail.com>
2024-11-06 17:51:29 +0900

    241106 Offline Class Code

---
+++ zyx_241106_오프라인/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>App.vue 파일임</h2>
+    <button :class="{dark:!flag[0], light:flag[0]}" @click="show(0)">0번 컴포넌트</button>
+    <button :class="{dark:!flag[1], light:flag[1]}" @click="show(1)">1번 컴포넌트</button>
+    <button :class="{dark:!flag[2], light:flag[2]}" @click="show(2)">2번 컴포넌트</button>
+    <main-page v-show="flag[0]"></main-page>
+    <Main2Page v-show="flag[1]"/>
+    <Main3Page v-show="flag[2]"/>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import MainPage from "./components/MainPage.vue";
+import Main2Page from "./components/Main2Page.vue";
+import Main3Page from "./components/Main3Page.vue";
+
+const flag = ref([true, true, true])
+const show = function (idx) {
+  flag.value[idx] = !flag.value[idx];
+}
+
+</script>
+
+<style scoped>
+.light {
+  background-color: lightblue;
+}
+
+.dark {
+  background-color: brown;
+}
+</style>
---
+++ zyx_241106_오프라인/src/components/MainPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>가수 정보 페이지에 오신 것을 환영합니다</h2>
+    <p>여러분이 좋아하는 가수와 아티스트에 대한 정보를 탐색해보세요</p>
+    <img src="@/assets/microphone.jpg" width=200 alt="마이크">
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style scoped>
+
+</style>
