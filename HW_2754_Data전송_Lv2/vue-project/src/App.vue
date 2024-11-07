46667178ccba34d3e599fcb983b9f6b993741301
JoonsunRyu <rjs7289@gmail.com>
2024-11-07 17:49:44 +0900

    241107 HW

---
+++ HW_2754_Data전송_Lv2/vue-project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ParentPage/>
+  </div>
+</template>
+
+
+<script setup>
+  import ChildPage from "./components/ChildPage.vue";
+  import ParentPage from "./components/ParentPage.vue";
+</script>
+
+
+<style scoped>
+  div {
+    border-style: solid;
+    border-width: 0.5px;
+    margin: 1%;
+    padding: 1%;
+  }
+</style>
---
+++ HW_2754_Data전송_Lv2/vue-project/src/components/ChildPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>자식 페이지입니다.</h3>
+    <p>이름: {{ name }}</p>
+    <p>나이: {{ age }}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    name: String,
+    age: Number,
+  })
+
+  import ParentPage from "./ParentPage.vue"
+
+</script>
+
+<style scoped>
+
+</style>
