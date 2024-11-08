d6f35815f9a520417875f7dfbf86abaea1285cab
JoonsunRyu <rjs7289@gmail.com>
2024-11-08 17:54:24 +0900

    241107 WS Lv2

---
+++ WS_2764_퀴즈_Lv2/quiz-app/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {RouterLink, RouterView} from 'vue-router'
+
+</script>
+
+
+<template>
+  
+  <div>
+    <h2>Quiz 퀴즈</h2>
+    <p>여기는 퀴즈의 나라! 놀라운 퀴즈들이 기다리고 있어요. 지금 바로 도전해보세요!</p>
+    
+    <div>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/quiz">Quiz</RouterLink>
+    </div>
+    <RouterView />
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ WS_2764_퀴즈_Lv2/quiz-app/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>여기는 HomeView</h2>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
